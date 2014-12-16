d01528ab25ab9c1af1e7b4b8ca6911e26aa9d78e
Saleh Albatati <0xack13@gmail.com>
2014-12-16 15:18:03 +0300

    Fix indentation

--- tests/test_all_of_the_units.py
+++ tests/test_all_of_the_units.py
@@ -2,13 +2,12 @@ import unittest
 from grouper import grouper
 
 class GrouperTest(unittest.TestCase):
-    def test(self):
-
-    	#for i in grouper(2, 'ABCDEFGHIJK'):
+	def test(self):
+		#for i in grouper(2, 'ABCDEFGHIJK'):
 		#	print i
 		expected_result = [['A', 'B'], ['C', 'D'], ['E', 'F'], ['G', 'H'], ['I', 'J'], ['K']]
 		results = list(grouper.grouper(2, 'ABCDEFGHIJK'))
 		self.assertEqual(expected_result, results)
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
