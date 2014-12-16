4ea4d99957f9efe50add4b7b8ad7da391c307372
Saleh Albatati <0xack13@gmail.com>
2014-12-16 11:43:49 +0300

    Add grouper func

---
+++ grouper/grouper.py
@@ -0,0 +1,18 @@
+### >>> for i in grouper(2, 'ABCDEFG'):
+### ...     print i
+### ... 
+### ['A', 'B']
+### ['C', 'D']
+### ['E', 'F']
+##  ['G']
+
+def grouper(n, iterable):
+  "grouper(3, 'ABCDEFG') --> ABC DEF G"
+  buf = []
+  for i, c in enumerate(iterable):
+    buf.append(c)
+    if (i+1) % n == 0:
+      yield buf
+      buf = []
+  if buf:
+    yield buf
