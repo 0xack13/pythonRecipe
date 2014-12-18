48ab2313456885bf65673e027a904b3ca7a87af4
Saleh Albatati <0xack13@gmail.com>
2014-12-19 00:29:56 +0300

    Test decorator

--- tests/test_all_of_the_units.py
+++ tests/test_all_of_the_units.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 sys.path.append('../')
 from pythonRecipe.grouper import grouper
+from pythonRecipe.decorator import deco
 
 class GrouperTest(unittest.TestCase):
 	def test(self):
@@ -10,6 +11,12 @@ class GrouperTest(unittest.TestCase):
 		expected_result = [['A', 'B'], ['C', 'D'], ['E', 'F'], ['G', 'H'], ['I', 'J'], ['K']]
 		results = list(grouper.grouper().grouperfn(2, 'ABCDEFGHIJK'))
 		self.assertEqual(expected_result, results)
+	def test_decorator(self):
+		@deco.time_dec
+		def printFunction(n):
+			print "Here is my func!"
+		printFunction(10)
+		self.assertEqual(0, 0)
 
 if __name__ == '__main__':
 	unittest.main()
